2a237fb67c0f9489ed5ee2faeb022c9b36a572ae
Isaac Lins <contact@isaaclins.com>
2025-11-06 15:47:15 +0100

    added base repo for 06

---
+++ 06/450-tdd-exercise/src/test/java/ch/bbw/TicTacToeTest.java
@@ -0,0 +1,15 @@
+package ch.bbw;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class TicTacToeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
